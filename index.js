5402815921e620612a3c18d81b712f08f6fa41bf
tushardhamani <34691076+tushardhamani@users.noreply.github.com>
2018-03-10 00:48:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-// basic example of d3 data binding
-// see detailed explaination of how it works https://bost.ocks.org/mike/join/
-var theData = [ 1, 2, 3 ]
+var width = 960, height = 500;
+    var color = d3.scale.category20();
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-var p = d3.select("body").selectAll("p") // returns empty selection
-    .data(theData) //selection is joined to an array
-    .enter() //3 possible states (enter / update / exit) because this is empty we use enter to return selection
-    .append("p") //add p tag to each data element
-    .text("hello "); //attr of p tag
+d3.csv('courses.csv', function(data) {
 
-// javascript anonymous functions
-// javascript reserved i and this
-// var p = d3.select("body").selectAll("p")
-//     .data(theData)
-//     .enter()
-//     .append("p")
-//     .text(function(d) {
-//         return d
-//     });
+    var nodes = [], edges = [];
 
+    for (var k = 0, n = data.length; k < n; k += 1) {
+        nodes.push({name: data[k].name, size: data[k].size, id: data[k].id}),
+        edges.push({source: data[k].id, target: data[k].parent});       
+    }
+});
 
-//.data no key so you can pass array d3 generates key for you
-//.enter placeholder for missing elements
+    var force = d3.layout.force()
+       .nodes(nodes
+       .links(edges)
+       .size([width, height])
+       .start();
 
-// Instead of telling D3 how to do something, tell D3 what you want.
+    var link = svg.selectAll(".link")
+        .data(edges)
+        .enter().append("line")
+        .attr("class", "link")
+
+    var node = svg.selectAll(".node")
+        .data(nodes)
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", nodes.size)
